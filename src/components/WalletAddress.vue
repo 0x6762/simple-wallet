55013864762789b30f4172042765cde2e1f891b0
Gabs <gabs@fundz.money>
2023-12-13 19:38:09 -0300

    Display name and address inline

--- src/components/WalletAddress.vue
+++ src/components/WalletAddress.vue
@@ -6,7 +6,12 @@
       v-model="inputValue"
       @keyup.enter="updateWalletAddress"
     />
-    <p class="wallet-address">{{ walletAddress }}</p>
+    <div class="wrap-content">
+      <p class="wallet-name">My wallet</p>
+      <p class="wallet-address">
+        {{ walletAddress.slice(0, 5) + '...' + walletAddress.slice(-5) }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -24,4 +29,26 @@ const updateWalletAddress = () => {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrap-content {
+  display: flex;
+  align-items: center;
+  margin-top: 24px;
+  padding: 16px;
+  gap: 16px;
+
+  .wallet-name {
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--white);
+  }
+  .wallet-address {
+    font-size: 14px;
+    font-weight: 400;
+    color: var(--gray600);
+    border: 1px solid var(--color-surface);
+    padding: 8px 12px;
+    border-radius: 100px;
+  }
+}
+</style>
