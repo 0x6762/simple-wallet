bc997851f773efb3dc393fa0f321dc4a2ef0084c
Gabs <gabs@fundz.money>
2023-12-13 17:32:38 -0300

    Save to local storage

--- src/components/WalletAddress.vue
+++ src/components/WalletAddress.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="wrap-wallet-address">
-    <p class="wallet-address">Wallet Address will appear here</p>
+    <input
+      type="text"
+      placeholder="Input an address..."
+      v-model="inputValue"
+      @keyup.enter="updateWalletAddress"
+    />
+    <p class="wallet-address">{{ walletAddress }}</p>
   </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+// Initialize walletAddress with value from local storage or an empty string
+const walletAddress = ref(localStorage.getItem('walletAddress') || '')
+const inputValue = ref('')
+
+const updateWalletAddress = () => {
+  walletAddress.value = inputValue.value
+}
+
+watchEffect(() => {
+  // Store walletAddress in local storage whenever it changes
+  localStorage.setItem('walletAddress', walletAddress.value)
+})
+</script>
+
 <style lang="scss" scoped></style>
